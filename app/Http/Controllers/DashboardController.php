3db419fd6d1b241826df865678ff320edcc054e6
zakydfls <111202113316@mhs.dinus.ac.id>
2024-08-26 17:19:45 +0700

    add wallet transaction module UI

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TransactionWallet;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-    //
+    public function wallet()
+    {
+        $user = Auth::user();
+
+        // topup, withdrawal, revenue, expense
+        $wallet_transaction = TransactionWallet::where("user_id", $user->id)
+            ->orderBy('id')
+            ->paginate(10);
+
+        return view('dashboard.wallet', compact('wallet_transaction'));
+    }
 }
